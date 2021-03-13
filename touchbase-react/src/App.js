e59513646cc9937935172cb7ec37887057b7bc1a
Jahnavi Bavuluri <jbavuluri@gmail.com>
2021-03-13 00:37:21 -0500

    first commit

---
+++ touchbase-react/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { LandingPage } from './components/LandingPage'
+
+function App() {
+  const [landingPage, setLandingPage] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      console.log(data.welcome);
+      setLandingPage(data.welcome);
+    });
+  }, []);
+
+  console.log(landingPage)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LandingPage landingPage = {landingPage}/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+/*
+function App() {
+  const [landingPage, setLandingPage] = useState([]);
+
+  useEffect(() => {
+    fetch("/").then(response =>
+      response.json().then(data => {
+        setLandingPage(data);
+      })
+    );
+  }, []);
+
+  console.log(setLandingPage);
+
+  return (
+    <div className="App">
+      <LandingPage landingPage = {landingPage}/>
+    </div>
+  );
+}
+
+export default App;
+*/
---
+++ touchbase-react/src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from 'touchbase-react/src/logo.svg';
+//import 'touchbase-react/src/App.css';
+
+export const LandingPage = ({ landingPage }) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{landingPage}</p>
+      </header>
+    </div>
+  );
+}
